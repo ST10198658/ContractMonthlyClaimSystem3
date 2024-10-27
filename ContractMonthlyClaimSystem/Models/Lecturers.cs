a1ddaa82018ff795e520452e6ec98ed5ffd9b331
Kamogelo Pitjeng <ST10198658@rcconnect.edu.za>
2024-10-27 19:17:31 +0000

    Add project files.

---
+++ ContractMonthlyClaimSystem/Models/Lecturers.cs
@@ -0,0 +1,19 @@
+﻿namespace ContractMonthlyClaimSystem.Models
+{
+    public class Lecturers
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Phone {  get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Designation { get; set; }
+
+       
+
+    }
+}
