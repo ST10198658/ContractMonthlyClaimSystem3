ee1b035c5ed554429ea23c073a1d360567137b66
Kamogelo Pitjeng <ST10198658@rcconnect.edu.za>
2024-11-14 14:17:14 +0000

    Create bank and SystemCodeDetail class

---
+++ ContractMonthlyClaimSystem/Models/Bank.cs
@@ -0,0 +1,14 @@
+﻿namespace ContractMonthlyClaimSystem.Models
+{
+    public class Bank
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string AccountNum { get; set; }
+
+    }
+}
